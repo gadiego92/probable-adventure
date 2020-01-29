c3952d03bf688897e5bdce5dd845006147b0f5b7
Diego Gonzalez <gadiego92@gmail.com>
2020-01-29 10:42:22 +0100

    Add motor controller node. Closes #16.

---
+++ catkin_ws/src/rover/scripts/motor_controller.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+import time
+
+import rospy
+
+from lewansoul_wrapper import MotorControllers
+from rover_msgs.msg import Commands
+
+motor_controller = MotorControllers()
+mutex = False
+
+
+def callback(command_message):
+	"""
+	Callback function called when a Commands message is received from /commands_topic topic
+
+	:param list command_message: A list of corner and motor lists values
+	"""
+
+	global mutex
+
+	rospy.loginfo(command_message)
+	
+	while mutex:
+		time.sleep(0.001)
+		print "command_message are being buffered"
+	
+	mutex = True
+
+	# Send angle values to corner motors
+	motor_controller.cornerToPosition(command_message.corner_motor)
+
+	# Send speed values to drive motors
+	motor_controller.sendMotorDuty(command_message.drive_motor)
+	
+	mutex = False
+
+def shutdown():
+	"""
+	Stop motors
+	"""
+
+	print "Killing motors"
+	motor_controller.killMotors()
+
+if __name__ == "__main__":
+	rospy.init_node("motor_controller_node")
+	rospy.loginfo("Starting the motor_controller node")
+	rospy.on_shutdown(shutdown)
+
+	sub = rospy.Subscriber("/commands_topic", Commands, callback)
+
+	rate = rospy.Rate(5)
+	
+	rospy.spin()
