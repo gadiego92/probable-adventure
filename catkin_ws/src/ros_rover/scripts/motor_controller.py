c3b35bfbf8d39f35c99985ac8c2a0fa09900c445
Diego Gonzalez <gadiego92@gmail.com>
2020-05-11 19:22:23 +0200

    Fix format.

--- catkin_ws/src/ros_rover/scripts/motor_controller.py
+++ catkin_ws/src/ros_rover/scripts/motor_controller.py
@@ -11,45 +11,47 @@ mutex = False
 
 
 def callback(command_message):
-	"""
-	Callback function called when a Commands message is received from /commands_topic topic
+    """
+    Callback function called when a Commands message is received from /commands_topic topic
 
-	:param list command_message: A list of corner and motor lists values
-	"""
+    :param list command_message: A list of corner and motor lists values
+    """
 
-	global mutex
+    global mutex
 
-	rospy.loginfo(command_message)
+    rospy.loginfo(command_message)
 
-	while mutex:
-		time.sleep(0.001)
-		print "command_message are being buffered"
+    while mutex:
+        time.sleep(0.001)
+        print "command_message are being buffered"
 
-	mutex = True
+    mutex = True
 
-	# Send angle values to corner motors
-	motor_controller.cornerToPosition(command_message.corner_motor)
+    # Send angle values to corner motors
+    motor_controller.cornerToPosition(command_message.corner_motor)
 
-	# Send speed values to drive motors
-	motor_controller.sendMotorDuty(command_message.drive_motor)
+    # Send speed values to drive motors
+    motor_controller.sendMotorDuty(command_message.drive_motor)
 
-	mutex = False
+    mutex = False
+
 
 def shutdown():
-	"""
-	Stop motors
-	"""
+    """
+    Stop motors
+    """
 
-	print "Killing motors"
-	motor_controller.killMotors()
+    print "Killing motors"
+    motor_controller.killMotors()
+
 
 if __name__ == "__main__":
-	rospy.init_node("motor_controller_node")
-	rospy.loginfo("Starting the motor_controller node")
-	rospy.on_shutdown(shutdown)
+    rospy.init_node("motor_controller_node")
+    rospy.loginfo("Starting the motor_controller node")
+    rospy.on_shutdown(shutdown)
 
-	sub = rospy.Subscriber("/commands_topic", Commands, callback)
+    sub = rospy.Subscriber("/commands_topic", Commands, callback)
 
-	rate = rospy.Rate(5)
+    rate = rospy.Rate(5)
 
-	rospy.spin()
+    rospy.spin()
